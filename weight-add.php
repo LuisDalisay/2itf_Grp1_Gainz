1e5e68fab91c75cc6ba2c9d1f0532dee103f84d7
AngeloDomingo <124044199+AngeloDomingo@users.noreply.github.com>
2023-05-21 23:08:53 +0800

    weight add

---
+++ weight-add.php
@@ -0,0 +1,86 @@
+<?php
+@include "config.php";
+
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header('location:index.php');
+}
+
+if (isset($_POST["submit"])) {
+    $current_weight = $_POST['current_weight'];
+    $weight_id = $_SESSION['user_id'];
+    $weight_goal = $_POST['weight_goal'];
+
+    $sql = "INSERT INTO `user_goals`(`id`, `current_weight`, `weight_goal`, `weight_id`, `date-added`) VALUES (NULL,'$current_weight', '$weight_goal', '$weight_id', NOW())";
+
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        header("Location: weight.php?msg=New record created successfully");
+    } else {
+        echo "Failed: " . mysqli_error($conn);
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/styles-goals.css">
+
+    <title>Gainz Tracker</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="text-center mb-4">
+            <h3>Add New Record</h3>
+            <p>Complete the form below to add a new record</p>
+        </div>
+
+        <?php
+        $weight_id = $_SESSION['user_id'];
+        $sql = "SELECT * FROM `user_goals` WHERE weight_id = $weight_id"; // you're the problem
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        ?>
+
+        <div class="container-main d-flex justify-content-center">
+            <form action="" method="post" style="width:50vw; min-width:300px;">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label class="form-label">Current Weight(lbs):</label>
+                        <input type="number" class="form-control" name="current_weight" placeholder="215" required>
+                        <input type="hidden" class="form-control" name="weight_goal" value="<?php
+                                                                                            if (empty($row['weight_goal'])) {
+                                                                                                echo 0;
+                                                                                            } else {
+                                                                                                echo $row['weight_goal'];
+                                                                                            } ?>">
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" class="btn btn-success" name="submit">Save</button>
+                    <a href="weight.php" class="btn btn-danger">Cancel</a>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
