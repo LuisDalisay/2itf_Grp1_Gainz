6cf4f22f18525832d1eea5a29b1e4c0f40e0a6d7
LuisDalisay <124044185+LuisDalisay@users.noreply.github.com>
2023-05-22 16:17:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -29,3 +29,24 @@ if (isset($_POST['register'])) {
         }
     }
 };
+
+if (isset($_POST['submit'])) {
+
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = md5($_POST['password']);
+
+    $select = " SELECT * FROM gainz_users WHERE email = '$email' && password = '$pass' ";
+
+    $result = mysqli_query($conn, $select);
+
+    if (mysqli_num_rows($result) > 0) {
+
+        $row = mysqli_fetch_array($result);
+
+        $_SESSION['user_name'] = $row['name'];
+        $_SESSION['user_id'] = $row['id'];
+        header('location:main_page.php');
+    } else {
+        $error[] = 'incorrect email or password!';
+    }
+};
