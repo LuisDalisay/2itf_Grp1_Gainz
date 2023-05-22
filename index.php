4bb53abafb874b9e58a10eaa175fee2ab8b6ac79
LuisDalisay <124044185+LuisDalisay@users.noreply.github.com>
2023-05-22 17:03:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -141,7 +141,20 @@ if (isset($_POST['submit'])) {
         </div>
     </div>
 
+    <script>
+        const container = document.getElementById('container');
+        const overlayCon = document.getElementById('overlayCon');
+        const overlayBtn = document.getElementById('overlayBtn');
 
+        overlayBtn.addEventListener('click', () => {
+            container.classList.toggle('right-panel-active');
+
+            overlayBtn.classList.remove('btnScaled');
+            window.requestAnimationFrame(() => {
+                overlayBtn.classList.add('btnScaled');
+            })
+        });
+    </script>
 
 </body>
 
