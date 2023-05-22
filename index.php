7939e12bcdcfb831d6a716996bd471fe6914b40e
LuisDalisay <124044185+LuisDalisay@users.noreply.github.com>
2023-05-22 16:25:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -50,3 +50,55 @@ if (isset($_POST['submit'])) {
         $error[] = 'incorrect email or password!';
     }
 };
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gainz Tracker</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style-index.css">
+</head>
+
+<body>
+
+    <video autoplay loop muted plays-inline class="back-video">
+        <source src="./images/workout.mp4" type="video/mp4">
+    </video>
+
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="" method="post">
+                <h1>Sign in</h1>
+                <?php
+                if (isset($error)) {
+                    foreach ($error as $error) {
+                        echo '<span class="error-msg">' . $error . '</span>';
+                    };
+                };
+                ?>
+                <div class="infield">
+                    <input type="email" name="email" required placeholder="enter your email">
+                    <label></label>
+                </div>
+                <div class="infield">
+                    <input type="password" name="password" required placeholder="enter your password">
+                    <label></label>
+                </div>
+                <input type="submit" name="submit" value="login now" class="form-btn">
+            </form>
+        </div>
+
+    </div>
+
+
+
+</body>
+
+</html>
